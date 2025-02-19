47a4c2f028fea2e42a8cf759d30045cf6436eb15
Vitor Cecato <vitor.cecatosesi@gmail.com>
2025-02-19 14:34:26 -0300

    Adicionando App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,62 @@ function App() {
 
   return (
     <>
-    
+      <Header/>
+      <section className="meioPagina">
+        <main className="elementosCartas">
+          <section className="blocoCartas">
+            <Perfil
+              // Aqui estamos passando a foto como propriedade para o componente "Perfil"
+              foto={fotoPerfilLauraB}
+              // Aqui estamos passando o nome como propriedade para o componente "Perfil"
+              nome="Laura Betti Migliaccio"
+              // Aqui estamos passando os hobbies como propriedade para o componente "Perfil"
+              hobbies="Os meus hobbies são: Ouvir música e ver filmes/séries."
+            />
+
+            <Perfil 
+              foto={fotoPerfilLucas} 
+              nome="Lucas Casagrande da Silva" 
+              hobbies="Os meus hobbies são: Praticar vôlei, jogar jogos online e assistir séries." 
+            />
+
+            <Perfil 
+              foto={fotoPerfilMilena} 
+              nome="Milena Oliveira Souza" 
+              hobbies="Os meus hobbies são: Escutar música, ver filmes e séries, estudar matemática e cozinhar." 
+            />
+          </section>
+
+          <section className="blocoCartas">
+            <Perfil 
+              foto={fotoPietro} 
+              nome="Pietro Melle Michelin" 
+              hobbies="Os meus hobbies são: Praticar esportes" 
+            />
+
+            <Perfil 
+              foto={fotoPyetro} 
+              nome="Pyetro Joaquim Taborda Nunes" 
+              hobbies="Os meus hobbies são: Jogar jogos online, ouvir música e cozinhar" 
+            />
+
+            <Perfil 
+              foto={fotoVitor} 
+              nome="Vitor Geraldo Cecato" 
+              hobbies="Meus hobbies são: jogar jogos online, programar e nadar." 
+            />
+          </section>
+        </main>
+        <Valores  valor1="União" valor2="Honestidade" valor3="Comprometimento"/>
+      </section>
+      <Footer 
+        imagem1={foto} 
+        imagem2={foto2} 
+        email="vitor.cecatosesi@gmail.com" 
+        instagram="@vitorgcecato"
+        texto="O Vitor Geraldo Cecato é o responsável pelo projeto PROPS, cuidando da coordenação e garantindo que o conteúdo seja de qualidade e esteja sempre no caminho certo." 
+        imglogo={logo}
+      />
     </>
   )
 }
