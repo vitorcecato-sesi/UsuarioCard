bc402ae5db6458e211f1ad68ccb84dd0650ee940
Vitor Cecato <vitor.cecatosesi@gmail.com>
2025-02-19 14:33:37 -0300

    Adicionando components e imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 import './App.css'
 
+// Componentes
+  import Perfil from "./components/Perfis"
+  import Header from "./components/Header"
+  import Valores from "./components/aside"
+  import Footer from "./components/Footer"
+//.
+
+// Fotos
+  import fotoPerfilLauraB from "./assets/fotoUsuarioLauraB.png"
+  import fotoPerfilLucas from "./assets/fotoUsuarioLucas.png"
+  import fotoPerfilMilena from "./assets/fotoUsuarioMilena.png"
+  import fotoPietro from "./assets/fotoUsuarioPietroM.png"
+  import fotoPyetro from "./assets/fotoUsuarioPyetro.png"
+  import fotoVitor from "./assets/fotoUsuarioVitorG.png"
+//.
+
+// Fotos Footer
+  import foto from "./assets/gmail.png"
+  import foto2 from "./assets/instagram.png"
+  import logo from "./assets/logoG3.png"
+//.
+
 function App() {
 
   return (
